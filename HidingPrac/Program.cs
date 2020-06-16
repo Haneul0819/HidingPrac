b7fbfb0e764b05478d9accf8eaa2d98bb78d7534
Haneul0819 <s2018s08@e-mirim.hs.kr>
2020-06-16 15:01:36 +0900

    higing 예제

--- HidingPrac/Program.cs
+++ HidingPrac/Program.cs
@@ -8,8 +8,30 @@ namespace HidingPrac
 {
     class Program
     {
+        class Parent
+        {
+            public int variable = 273;
+            public virtual void Method()
+            {
+                Console.WriteLine("부모메서드입니다.");
+            }
+        }
+
+        class Child : Parent
+        {
+            public new string variable = "shadowing";
+            public override void Method()
+            {
+                Console.WriteLine("자식메서드입니다.");
+            }
+        }
         static void Main(string[] args)
         {
+            Child c = new Child();
+            Console.WriteLine(" c = " + c.variable);
+            c.Method();
+            Console.WriteLine(" 부모취급 c = " + ((Parent)c).variable);
+            ((Parent)c).Method();
         }
     }
 }
